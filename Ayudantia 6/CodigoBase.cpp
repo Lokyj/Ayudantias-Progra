20d20e923e99712d411bfc618f655ecaf8570f0d
Lokyj <javier.orestes@mail.udp.cl>
2025-05-28 02:08:22 -0400

    o_O

---
+++ Ayudantia 6/CodigoBase.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Gestión de productos
+void agregarProducto(string nombresProd[50], int preciosProd[50], float descuentosProd[50], int stockProd[50],  string nombre, int precio, int stock);
+int buscarProducto(string nombresProd[50],  string nombre);
+void buscarProductoPorIndice(string nombresProd[50],  int indiceProducto);
+void mostrarProductos(string nombresProd[50], int preciosProd[50], float descuentosProd[50], int stockProd[50]);
+bool agregarDescuento(string nombresProd[50], float descuentosProd[50], int numProductos, string nombre, float nuevoDescuento);
+
+// Gestión de ventas
+void registrarVenta(string nombresProd[50], int preciosProd[50], float descuentosProd[50], int stockProd[50],int ventas[200][3],string nombreProducto, int cantidad);
+
+float totalRecaudadoProducto(int ventas[200][3], int indiceProducto);
+int productoConLaMayorVenta(int ventas[200][3]);
+float porcentajeProductoSobreTotal(int ventas[200][3], int indiceProducto);
+
+int main() {
+    // Declaración de arreglos vacios
+    string nombresProd[50] = {}; // igualarlo a {} crea un arreglo completamente de cadenas vacias ("")
+    int preciosProd[50] = {}; // llena completamente de 0
+    float descuentosProd[50] = {};
+    int stockProd[50] = {};
+    int ventas[200][3] = {};
+
+    return 0;
+}
